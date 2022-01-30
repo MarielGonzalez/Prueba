e1894e844f0388c4d75c29bb0e8eef23421bf46f
MarielGonzalez <47929216+MarielGonzalez@users.noreply.github.com>
2022-01-30 18:47:30 +0000

    hols

--- Main.java
+++ Main.java
@@ -6,6 +6,14 @@ class Main {
     {
 		 return option.equals("G");           
     }
+
+		public static int randomNumbers()
+    {
+			int min=1; 
+			int max=100;
+		  int random = (int)Math.floor(Math.random()*(max+min+1)*min); 
+			return random;       
+    }
 	
 	public static void validarPlaca(String option)
     {
@@ -56,7 +64,7 @@ class Main {
 			validarPlaca(placa3);
 			
 			//System.out.printf("Placas de los Vehiculos Ingresados: %s y %s \n" , placa, placa2);
-			if(esUnaJeepeta(inicialPlaca)||esUnaJeepeta(inicialPlaca2) ) {
+			if(esUnaJeepeta(inicialPlaca)||esUnaJeepeta(inicialPlaca2) || esUnaJeepeta(inicialPlaca3) ) {
 				System.out.println("En estas placas hay una o mas Jeepeta(s)");
 				result = true;
 			}else {
@@ -64,64 +72,18 @@ class Main {
 				while(!end) {
 				System.out.println("Entre estas placas no hay una Jeepeta");
 				System.out.println("Generando numeros aleatorios...");
-				int max = 100;
-				int random1 = (int)Math.floor(Math.random()*(max));
-				int random2 = (int)Math.floor(Math.random()*(max));
-				int random3 = (int)Math.floor(Math.random()*(max));
-				System.out.println(random1 + "\n" + random2 + "\n" + random3);
-				
-				  if((random1 <= 3 && random1 <=10))  {
-					  System.out.println("Se encontro numero aleatorio entre 3 y 10...");
-					  for(int r=1; r<=random1; r++ ) {
-						    System.out.println("");
-							System.out.printf("%d",r);
-							;
-							   for(int i=1; i<6; i++) {
-								
-								System.out.printf("%d",i);
-													
-							}
-								System.out.println("\n");			
-							
-						   }	end = true;
-					  
-					  
-				          } else 
-				  if((random2 <= 3 && random2 <=10))  {
-					  System.out.println("Se encontro numero aleatorio entre 3 y 10...");
-					  for(int r=1; r<=random2; r++ ) {
-						    System.out.println("");
-							System.out.printf("%d",r);
-							;
-							   for(int i=1; i<6; i++) {
-								
-								System.out.printf("%d",i);
-													
-							}
-								System.out.println("\n");			
-							
-						   }	end = true;
-					  
-					  
-				          }else 
-				  if((random3 <= 3 && random3 <=10))  {
-					  System.out.println("Se encontro numero aleatorio entre 3 y 10...");
-					  for(int r=1; r<=random3; r++ ) {
-						    System.out.println("");
-							System.out.printf("%d",r);
-							;
-							   for(int i=1; i<6; i++) {
-								
-								System.out.printf("%d",i);
-													
-							}
-								System.out.println("\n");			
-							
-						   }	end = true;
-					  
-					  
-				          }
+
+
+					for(int i =1; i>3; i++){
+						  int number=randomNumbers();
+							if (number <= 3 && number <=10){
+           System.out.println("Generando Secuencia...");
+
+				}  		
+
+
 				}
+				
 				
 				
 				
@@ -171,5 +133,6 @@ class Main {
 	
 
   }
+ }
  }
 
